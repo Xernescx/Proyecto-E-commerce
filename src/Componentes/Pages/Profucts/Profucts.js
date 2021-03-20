28f0841ba20d75ae045bd91f1211c927c47870d1
carlos <carlosjt@gmail.com>
2021-03-20 20:01:04 +0100

    BarOk

--- src/Componentes/Pages/Profucts/Profucts.js
+++ src/Componentes/Pages/Profucts/Profucts.js
@@ -14,6 +14,7 @@ const Home = () => {
                 justify="center"
                 alignItems="center"
             > 
+                
                 <hr/>
                 <Products />
             </Grid>
