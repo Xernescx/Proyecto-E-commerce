263191fe131b9e07f3584b1b09d7031567ef280b
carlos <carlosjt@gmail.com>
2021-06-05 20:26:24 +0200

    Pago implementado

--- src/Componentes/Carrito/Carrito.js
+++ src/Componentes/Carrito/Carrito.js
@@ -10,7 +10,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import DeleteSweepIcon from '@material-ui/icons/DeleteSweep';
 import IconButton from '@material-ui/core/IconButton';
-
+import PaypalButton from '../Paypal/Paypal';
 
 const theme = createMuiTheme({
   palette: {
@@ -36,7 +36,6 @@ export default function SimpleContainer() {
   const userJ = JSON.parse(window.sessionStorage.getItem("user"));
   const [links, setLink] = useState([]);
   const [id, setID] = useState({});
-  const [price, setPrice] = useState([]);
   const [loading, setloading] = useState(true);
   const [totalPrice, setTotalPrice] = useState(0);
 
@@ -61,7 +60,7 @@ export default function SimpleContainer() {
         querySnapshot.forEach((doc) => {
 
           doc.data().carrito.forEach(element => {
-          
+
 
             db.collection("VideoGames")
               .where("name", "==", element).get().then((querySnapshot) => {
@@ -69,7 +68,7 @@ export default function SimpleContainer() {
                   /* console.log(doc1.data()) */
                   total = parseFloat(doc1.data().price) + parseFloat(total)
                   setTotalPrice(total)
-                  games.push({ name: doc1.data().name, price: doc1.data().price})
+                  games.push({ name: doc1.data().name, price: doc1.data().price })
                   setLink(links => [...links, doc1.data()])
                 });
               });
@@ -83,6 +82,12 @@ export default function SimpleContainer() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const order = {
+    customer: userJ.email,
+    total: parseFloat(totalPrice),
+    items: id
+  }
 
   function removeItemFromArr(arr, item) {
     var i = arr.indexOf(item);
@@ -107,13 +112,13 @@ export default function SimpleContainer() {
     setloading(false);
   };
 
-  const comprar = () => {
+  /* const comprar = () => {
 
     db.collection("pedidos").add({
       user: db.collection("users").doc(firebase.auth().currentUser.uid),
       date: new Date(),
       total: parseFloat(totalPrice),
-      games: id,
+      games: id
 
 
     }).then(() => {
@@ -140,7 +145,7 @@ export default function SimpleContainer() {
         console.error("Error updating document: ", error);
       });
 
-  }
+  } */
 
   const login = React.useCallback(async () => {
     console.log('Login')
@@ -218,7 +223,17 @@ export default function SimpleContainer() {
 
 
         <div className="carritoTotal">
-          <button className="" onClick={comprar} >Comprar</button>
+          {links.length === 0 && (
+            <p></p>
+          )}
+
+          {links.length > 0 && (
+            <div className="pagarButton">
+            <PaypalButton order={order} />
+            </div>
+            )}
+          
+
           <p>Total: {totalPrice} €</p>
 
         </div>
---
+++ src/Componentes/Paypal/Paypal.js
@@ -0,0 +1,123 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { db, auth } from '../FireBase/Firebase'
+import firebase from 'firebase/app';
+
+
+const PaypalClass = ({ order }) => {
+
+    const paypalConf = {
+        currency: 'EUR',
+        env: 'sandbox',
+        cliente: {
+            sandbox: 'AXANqxXyUpN9qBMRb5wZEXFmBDXYMr3vkA4XlkTn7-noKsMlb5f6ljUupgvTiFbqwy9vFJY9y3vkgVer',
+            production: '-- id--',
+
+        },
+        style: {
+            size: 'responsive',
+            color: 'black',
+            shape: 'pill',
+            label: 'checkout',
+        }
+    };
+
+    const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
+    const payment = (data, actions) => {
+        const payment = {
+            transaction: [
+                {
+                    amount: {
+                        total: order.total,
+                        currency: paypalConf.currency,
+                    },
+                    description: "Compra de mi tienda",
+                    custom: order.customer || '',
+                    item_list: {
+                        items: order.items
+                    }
+                }
+            ],
+            note_to_payer: 'Contactanos  para cualquier aclaracion'
+        }
+        return actions.payment.create({ payment })
+    }
+
+    const onApprove = (data, actions) => {
+        return actions.order.capture()
+            .then(response => {
+                /*   console.log(response) */
+                comprar()
+                alert(`El pago fue exitoso ${response.id}`)
+            })
+            .catch(error => {
+                console.log(error);
+                alert("Ocurrio un error al procesar el pago")
+            })
+    };
+
+
+    const onError = (error) => {
+        console.log(error);
+        alert("El pago no fue realizado, intentelo de nuevo ")
+    }
+
+    const onCancel = (data, actions) => {
+        alert("Pago no realizado, pago cancelado por el usuario")
+    }
+
+    const comprar = () => {
+
+        db.collection("pedidos").add({
+            user: db.collection("users").doc(firebase.auth().currentUser.uid),
+            date: new Date(),
+            total: parseFloat(order.total),
+            games: order.items
+
+
+        }).then(() => {
+            console.log("Document successfully updated!");
+
+        })
+            .catch((error) => {
+                // The document probably doesn't exist.
+                console.error("Error updating document: ", error);
+            });
+
+
+        db.collection("users").doc(firebase.auth().currentUser.uid).update({
+            carrito: [],
+
+
+        }).then(() => {
+            console.log("Document successfully updated!");
+            window.location = "/pedidos"
+        })
+            .catch((error) => {
+                // The document probably doesn't exist.
+                console.error("Error updating document: ", error);
+            });
+
+    }
+
+
+
+
+
+    return (
+        <PayPalButton
+            env={paypalConf.env}
+            client={paypalConf.cliente}
+            payment={(data, actions) => payment(data, actions)}
+            onApprove={(data, actions) => onApprove(data, actions)}
+            onCancel={(data, actions) => onCancel(data, actions)}
+            onError={(error) => onError(error)}
+            style={paypalConf.style}
+            commit
+            locale="es_ES"
+        />)
+
+};
+
+export default PaypalClass;
